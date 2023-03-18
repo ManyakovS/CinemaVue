7efb32bd08f0a7ea68a3192c97d138a44993a15c
Rafael2111 <117172668+Rafael2111@users.noreply.github.com>
2023-03-18 12:48:48 +0700

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,37 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const fs = require('fs')
+const path = require('path')
+
+const baseFolder =
+    process.env.APPDATA !== undefined && process.env.APPDATA !== ''
+        ? `${process.env.APPDATA}/ASP.NET/https`
+        : `${process.env.HOME}/.aspnet/https`;
+
+const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
+const certificateName = certificateArg ? certificateArg.groups.value : "vueapp";
+
+if (!certificateName) {
+    console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
+    process.exit(-1);
+}
+
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
+
+module.exports = {
+    devServer: {
+        https: {
+            key: fs.readFileSync(keyFilePath),
+            cert: fs.readFileSync(certFilePath),
+        },
+        proxy: {
+            '/V1': {
+                target: 'https://localhost:7172/',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/V1': ''
+                }
+            },
+        },
+        port: 5002
+    }
+}
